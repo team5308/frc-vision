ddcec552ab9541f5c1a647512be5a7ffee4c8f3e
Eric Frankel <efrankel624@gmail.com>
2017-02-07 22:29:16 -0800

    readability improvements added a lot of comments improved some variable names removed unused constants and added some new ones where necessary

--- cam_client.py
+++ cam_client.py
@@ -1,33 +1,34 @@
 import socket 
 import cv2
 import numpy as np
-import cPickle as pickle
 from time import sleep
-HOST = '172.24.2.76'
-PORT = 3000
-RECV_SIZE = 691200 
+SERVER_IP = '172.24.2.76'
+PORT = 3000 #port on which the server is listening
+HEADER_LEN = 16 #length of message describing the length of the actual image data
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #grab a socket
+sock.connect((HOST, PORT)) #connect to that server
 
 def recv_msg(sock, count):
-    data = b''
-    while len(data) < count:
-        packet = sock.recv(count - len(data))
-        if not packet:
+    """recieves an aribitrarily sized message over sock"""
+    data = b'' #we need a place to store the data
+    while len(data) < count: #make sure we grab all the data
+        packet = sock.recv(count - len(data))#grab as much as we can
+        if not packet: #if there isn't any data we have nothing to return
            return None
-        data += packet
+        data += packet #append new info
     return data
 
 while True:
-    msg_len = int(s.recv(16))
-    frame_data = recv_msg(s, msg_len)
-    frame_arr = np.fromstring(frame_data, np.uint8)
-    frame = cv2.imdecode(frame_arr, cv2.IMREAD_COLOR) 
-    if frame != None:
+    msg_len = int(sock.recv(HEADER_LEN))#get the length of the image data
+    frame_data = recv_msg(sock, msg_len)#grab the frame
+    frame_arr = np.fromstring(frame_data, np.uint8)#convert it to np array
+    frame = cv2.imdecode(frame_arr, cv2.IMREAD_COLOR)#decode it into something we can display
+    if frame != None:#error checking
         cv2.imshow('CLIENT', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):#allow user to quit the client program
         break
 
+#some cleanup stuff
 cv2.destroyAllWindows()
 s.close()
--- cam_server.py
+++ cam_server.py
@@ -2,24 +2,23 @@ import socket
 import cv2
 import numpy as np
 from time import sleep
-import cPickle as pickle
-PORT = 3000
-HOST = '172.24.2.76' 
-CAM_PORT = 0
-FPS = 24
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-(clientsock, addr) = s.accept()
+PORT = 3000 #port to connect over
+HOST = '172.24.2.76' #host IP to use
+CAM_PORT = 0 #default camera to use capture
+FPS = 24 #frames per second
+HEADER_LEN = 16 #longest possible length of encoded string
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #init socket
+sock.bind((HOST, PORT)) #bind that socket
+sock.listen(1) #listen (we only want 1 connection)
+(client, addr) = s.accept() #wait for our very special client to connect
 
-capture = cv2.VideoCapture(CAM_PORT)
-while True:
-    sleep(1.0/FPS)
-    ret, frame = capture.read()
-    width, height = frame.shape[:2]
-    small_frame = cv2.resize(frame, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
-    sendable_frame = cv2.imencode('.jpg', small_frame)[1].tostring()
-    clientsock.send(str(len(sendable_frame)).ljust(16)+sendable_frame)
+capture = cv2.VideoCapture(CAM_PORT) #get an object so we can grab some frames
+while True: #infinite loops are good for servers
+    sleep(1.0/FPS)#control framerate
+    ret, frame = capture.read()#get a frame from the camera
+    small_frame = cv2.resize(frame, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)#shrink the frame so we use less bandwidth
+    sendable_frame = cv2.imencode('.jpg', small_frame)[1].tostring()#econde the shrunken frame and convert it to a string
+    client.send(str(len(sendable_frame)).ljust(HEADER_LEN)+sendable_frame)#send the length of the image and the iamge to the client
 
-clientsock.close()
-s.close()
+clientsock.close()#close the conneciton to the client
+s.close()#close the socket on the host
