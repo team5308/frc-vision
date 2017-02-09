ddb6ec47fe02c5376b072f0766fa99762b02998b
ericf123 <efrankel624@gmail.com>
2017-02-08 17:33:57 -0800

    mvp

--- cam_client.py
+++ cam_client.py
@@ -1,26 +1,40 @@
 import socket 
 import cv2
 import numpy as np
+import cPickle as pickle
 from time import sleep
 HOST = '172.24.2.76'
 PORT = 3000
-HEADER_LEN = 16
+RECV_SIZE = 691200 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-def recv_msg(sock):
-    """recieves 2 messages over conn, the first of which describes
-       the length of the following message. returns the second message"""
-    msg_len = int(sock.recv(HEADER_LEN))
-    print msg_len
-    return sock.recv(msg_len)
+def is_int(msg):
+    """Takes a message and returns whether or not the message is an int"""
+    try:
+        conv = int(msg)
+        return True
+    except:
+        return False
+
+
+def recv_msg(sock, count):
+    data = b''
+    while len(data) < count:
+        packet = sock.recv(count - len(data))
+        if not packet:
+           return None
+        data += packet
+    return data
 
 while True:
-    stringData = recv_msg(s)
-    data = np.fromstring(stringData, dtype='uint8')
-
-    decoded_img = cv2.imdecode(data, 1)
-    cv2.imshow('CLIENT', decoded_img)
+    msg_len = int(s.recv(16))
+    frame_data = recv_msg(s, msg_len)
+    frame_arr = np.fromstring(frame_data, np.uint8)
+    frame = cv2.imdecode(frame_arr, cv2.IMREAD_COLOR) 
+    if frame != None:
+        cv2.imshow('CLIENT', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
