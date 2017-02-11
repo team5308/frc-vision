09b5c19b48cff88f40a4301ee26e42f66a0cf4ab
Eric Frankel <efrankel624@gmail.com>
2017-02-11 00:38:31 +0000

    added analyze function uses hsv threshold to get reflective tape from frame analyzes the thresholded frame and places a bounding rect around larges contour

--- netcam.py
+++ netcam.py
@@ -17,9 +17,9 @@ HEADER_LEN = 16 #longest possible length of encoded string
 #v4l2-ctl -c exposure_auto=1 exposure_absolute=10#
 ##################################################
 
-def cam_attr_const(simple_name):
+def cv_const(simple_name):
     ocv3 = StrictVersion(cv2.__version__) >= StrictVersion('3.0.0') #are we using opencv3 or 2
-    return getattr(cv2 if ocv3 else cv2.cv, ("" if ocv3 else "CV_") + "CAP_PROP_" + simple_name)
+    return getattr(cv2 if ocv3 else cv2.cv, ("" if ocv3 else "CV_") + simple_name)
 
 class Server():
     """Netcam server class. Provides functionality for serving webcam video over network"""
@@ -52,7 +52,7 @@ class Server():
             sleep(1.0/FPS)#control framerate
             ret, frame = self.capture.read()#get a frame from the camera
             if frame is not None:
-                small_frame = cv2.resize(frame, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)#shrink the frame so we use less bandwidth
+                small_frame = cv2.resize(self.analyze(frame), None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)#shrink the frame so we use less bandwidth
                 sendable_frame = cv2.imencode('.jpg', small_frame)[1].tostring()#econde the shrunken frame and convert it to a string
                 try:
                     self.client.send(str(len(sendable_frame)).ljust(HEADER_LEN)+sendable_frame)#send the length of the image and the iamge to the client
@@ -65,6 +65,32 @@ class Server():
         self.create_and_bind_sock()
         self.wait_for_connection()
         self.serve_forever()
+
+    def analyze(self, frame):
+        """returns analyzed frame with bounding box drawn around reflective tape"""
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_tape_thresh = np.array([89, 39, 216])
+        upper_tape_thresh = np.array([130, 253, 255])
+
+        thresh_frame = cv2.inRange(hsv, lower_tape_thresh, upper_tape_thresh)
+        contours, hierarchy = cv2.findContours(thresh_frame, cv_const("RETR_EXTERNAL"), cv_const("CHAIN_APPROX_NONE"))
+        contours = np.array(contours)
+        if len(contours) > 0:
+            max_area_contour = self.filter_contours(contours)
+            rect = cv2.minAreaRect(max_area_contour)
+            box = cv2.cv.BoxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+        return frame
+
+    def filter_contours(self, contours):
+        #vectorized_cnt_area = np.vectorize(cv2.contourArea)
+        #contour_areas = vectorized_cnt_area(contours)
+        contour_areas = []
+        for cnt in contours:
+            contour_areas.append(cv2.contourArea(cnt))
+        return contours[np.argmax(contour_areas)]
+        
 
     def destroy(self, signum=None, frame=None):#three args are provided to comply with signal.signal interrupt handler
         """handler for keyboard interrupts or other events that 
@@ -138,6 +164,7 @@ class Client():
         else: 
             return True
         
+
     def destroy(self, signum=None, frame=None):
         """see Server.destroy"""
         print "Cleaning up..."
