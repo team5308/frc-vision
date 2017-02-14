8b7e8e7a1a2f18f81e0fc248002f1fae9d380d7e
Eric Frankel <efrankel624@gmail.com>
2017-02-14 02:46:59 +0000

    trying to fix opencv errors

--- analyzer.py
+++ analyzer.py
@@ -20,17 +20,19 @@ def analyze(frame):
     thresh_frame = cv2.inRange(hsv, lower_tape_thresh, upper_tape_thresh)
     contours = cv2.findContours(thresh_frame, cv_const("RETR_EXTERNAL"), cv_const("CHAIN_APPROX_NONE"))[1]
     contours = np.array(contours)
-    if len(contours) > 0:
+    if contours is not None:
         max_area_contour = filter_contours(contours)
-        rect = cv2.minAreaRect(max_area_contour)
-        rect_width_px = rect[0][0] - rect [1][0]
-        print "distance to tape: %f" % (distance_to_marker(TAPE_WIDTH_ACTUAL, rect_width_px))
-        if not hasattr(cv2, 'cv'):
-            box = cv2.boxPoints(rect)
-        else:
+        if max_area_contour is not None:
+            rect = cv2.minAreaRect(max_area_contour)
+            rect_width_px = rect[0][0] - rect [1][0]
+            print "distance to tape: %f" % (distance_to_marker(TAPE_WIDTH_ACTUAL, rect_width_px))
             box = cv2.cv.BoxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+#            if not hasattr(cv2, 'cv'):
+#                box = cv2.boxPoints(rect)
+#            else:
+#                box = cv2.cv.BoxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
     return frame
 
 def filter_contours(contours):
@@ -39,10 +41,13 @@ def filter_contours(contours):
        to find the contour with the maximum area"""
     #vectorized_cnt_area = np.vectorize(cv2.contourArea)
     #contour_areas = vectorized_cnt_area(contours)
-    contour_areas = []
-    for cnt in contours:
-        contour_areas.append(cv2.contourArea(cnt))
-    return contours[np.argmax(contour_areas)]
+    try:
+        contour_areas = []
+        for cnt in contours:
+            contour_areas.append(cv2.contourArea(cnt))
+        return contours[np.argmax(contour_areas)]
+    except:
+        return None
 
 def distance_to_marker(width_actual, width_px): #i forget how i got this formula, should probably figure it out
     return FOCAL_LENGTH * width_actual / width_px
