3110e7c78b54791e14ab0174ff13eca9a35597df
Eric Frankel <efrankel624@gmail.com>
2017-11-16 13:06:01 -0800

    adding comments and cleaning up file renamed all instances of MJPEG to MJPG switched DEFAULT_PORT (currently unused) to 5800 to comply with FRC FMS regulations

--- netcam.py
+++ netcam.py
@@ -8,17 +8,18 @@ from distutils.version import StrictVersion
 import Queue
 from threading import Thread
 import flask
-DEFAULT_PORT = 3000 #port to connect over
-CAM_NUMS = [0, 1]#default camera to use capture
+DEFAULT_PORT = 5800 #ports 5800 through 5810 are designated for team use in FMS whitepaper
+CAM_NUMS = [0]#default camera to use capture
 FPS = 24 #frames per second
+IMG_SIZE = (320,180) #(width, height) in pixels 
 
 #####################################################
 # command to properly adjust brightness run on tx1  #
 #v4l2-ctl -c exposure_auto=1 -c exposure_absolute=10#
 #####################################################
 
-frame_q = Queue.Queue(FPS)
-captures = []
+frame_q = Queue.Queue(FPS) #store our processed frames that are ready to transmit
+captures = [] #list of VideoCapture objects that refer to the cameras we have opened
 
 def start_captures(cam_nums):
     """start all of the video capture objects and store references to them in an instance variable"""
@@ -43,10 +44,13 @@ def process_frames(frames):
     for frame in frames:
         if frame is not None:
             #analyze the frame, then shrink it so we use less bandwidth
-            small_frame = cv2.resize(analyzer.analyze(frame), 
-                                     (320, 180),
+            small_frame = cv2.resize(frame,#analyzer.analyze(frame), 
+                                     IMG_SIZE,
                                      interpolation=cv2.INTER_AREA)
             processed_frames.append(small_frame)
+    #if we have more than one capture open, we will get more than one frame, so we stitch the images
+    #together by concatenating their representations as numpy arrays, then encode to JPG. The 
+    #concatenation axis determines horizontal or vertical stacking. 1 for horizontal, 0 for vertical.
     if len(processed_frames) > 1:
         retval, buf = cv2.imencode('.jpeg', np.concatenate(processed_frames, axis=1))
     else:
@@ -56,36 +60,37 @@ def process_frames(frames):
 def queue_frame(frame, q):
     try: 
         q.put_nowait(frame)#queue the processed frame to be sent to the client
+    #may need to be more proactive about managing the queue if bandwidth issues lead to frame dropping
     except Queue.Full:
         pass
 
 def capture_forever():
     """get a capture object and serve until we get an interrupt"""
     while True: #infinite loops are good for servers
-        sleep(1.0/FPS)#control framerate
+        #sleep(1.0/FPS)#control framerate
         frames = get_raw_frames(captures)#grab all of the raw frames from the capture objects
         processed_frame = process_frames(frames)#do CV processing, put frames together in single frame
-        queue_frame(processed_frame, frame_q)#queue the frame for MJPEG stream
+        queue_frame(processed_frame, frame_q)#queue the frame for MJPG stream
 
 app = flask.Flask('netcam')#create Flask app
 
-@app.route('/')
-def index(self):
-    return flask.render_template('index.html')
-
-def mjpeg_gen():
+def mjpg_gen():
+    """a generator that infinitely yields MJPG frames"""
     while True:
         frame = frame_q.get().tostring()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/feed') 
-def mjpeg_feed():
-    return flask.Response(mjpeg_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+def mjpg_feed():
+    """route for actual MJPG feed"""
+    return flask.Response(mjpg_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
+    #start capture thread
     captures = start_captures(CAM_NUMS)
-    t = Thread(target=capture_forever)
-    t.daemon = True
-    t.start()
+    cap_thread = Thread(target=capture_forever)
+    cap_thread.daemon = True
+    cap_thread.start()
+    #run the flask app to start the server
     app.run(host='172.24.4.74', debug=True)
