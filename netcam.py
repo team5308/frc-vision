880dff8d7ec72ef320eb2fa83005e4c54e72c561
Eric Frankel <efrankel624@gmail.com>
2017-11-16 13:27:23 -0800

    unfinished add argument parser

--- netcam.py
+++ netcam.py
@@ -9,6 +9,7 @@ import Queue
 from threading import Thread
 import flask
 DEFAULT_PORT = 5800 #ports 5800 through 5810 are designated for team use in FMS whitepaper
+DEFAULT_HOST = '172.24.4.74'
 CAM_NUMS = [0]#default camera to use capture
 FPS = 24 #frames per second
 IMG_SIZE = (320,180) #(width, height) in pixels 
@@ -52,9 +53,9 @@ def process_frames(frames):
     #together by concatenating their representations as numpy arrays, then encode to JPG. The 
     #concatenation axis determines horizontal or vertical stacking. 1 for horizontal, 0 for vertical.
     if len(processed_frames) > 1:
-        retval, buf = cv2.imencode('.jpeg', np.concatenate(processed_frames, axis=1))
+        retval, buf = cv2.imencode('.jpg', np.concatenate(processed_frames, axis=1))
     else:
-        retval, buf = cv2.imencode('.jpeg', processed_frames[0])
+        retval, buf = cv2.imencode('.jpg', processed_frames[0])
     return buf
 
 def queue_frame(frame, q):
@@ -67,12 +68,15 @@ def queue_frame(frame, q):
 def capture_forever():
     """get a capture object and serve until we get an interrupt"""
     while True: #infinite loops are good for servers
-        #sleep(1.0/FPS)#control framerate
-        frames = get_raw_frames(captures)#grab all of the raw frames from the capture objects
-        processed_frame = process_frames(frames)#do CV processing, put frames together in single frame
-        queue_frame(processed_frame, frame_q)#queue the frame for MJPG stream
+        try:
+            #sleep(1.0/FPS)#control framerate
+            frames = get_raw_frames(captures)#grab all of the raw frames from the capture objects
+            processed_frame = process_frames(frames)#do CV processing, put frames together in single frame
+            queue_frame(processed_frame, frame_q)#queue the frame for MJPG stream
+        except IndexError:#lazy error handling for else case in process_frames
+            pass
 
-app = flask.Flask('netcam')#create Flask app
+app = flask.Flask(__name__)#create Flask app
 
 def mjpg_gen():
     """a generator that infinitely yields MJPG frames"""
@@ -83,14 +87,21 @@ def mjpg_gen():
 
 @app.route('/feed') 
 def mjpg_feed():
-    """route for actual MJPG feed"""
+    """route for MJPG feed"""
     return flask.Response(mjpg_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    #start capture thread
-    captures = start_captures(CAM_NUMS)
+def start_cap_thread(cam_nums):
+    """Init and start the capture thread"""
+    captures = start_captures(cam_nums)
     cap_thread = Thread(target=capture_forever)
     cap_thread.daemon = True
     cap_thread.start()
-    #run the flask app to start the server
-    app.run(host='172.24.4.74', debug=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Vision server for FRC team 5835.')
+    parser.add_argument('bind_address', action='store', type=str, nargs='?', default=DEFAULT_HOST)
+    parser.add_argument('bind_port', action='store', type=str, nargs='?', default=DEFAULT_PORT)
+#    args = parser.parse_args()
+    start_cap_thread(CAM_NUMS)
+    #app.run(host=args.bind_address, port=args.bind_port) #run the flask app to start the server
+    app.run(host='localhost')
